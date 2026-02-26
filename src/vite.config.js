799f9acc14b0882ba48cff3d30a02b622f6b200f
yu-corder <yuuya.9602@icloud.com>
2026-02-26 10:39:52 +0900

    vite config update

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+    },
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/admin/admin.js',
             ],
             refresh: true,
         }),
